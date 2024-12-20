8c33407aa0662c5a073e81552698182afd6a3d2d
Matthieu Doutreligne <matt.dout@gmail.com>
2024-12-20 20:17:15 +0100

    wip cross validation intutition

--- mleco/figures/3_flexible learner.py
+++ mleco/figures/3_flexible learner.py
@@ -1,10 +1,14 @@
 # %%
+from IPython.extensions import autoreload
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
 import seaborn as sns
 
+from mleco.figures.utils import plot_train_test_indices
+from mleco.constants import DIR2FIG
+%load_ext autoreload
+%autoreload 2
 # %%
 from sklearn.datasets import fetch_openml
 
@@ -45,7 +49,7 @@ from sklearn.pipeline import make_pipeline
 
 X = df.drop(columns="WAGE")
 y = df["WAGE"]
-regressor = Lasso(alpha=0.1)
+regressor = Lasso(alpha=10)
 pipeline = make_pipeline(preprocessor, regressor)
 pipeline
 # %% [markdown]
@@ -53,17 +57,51 @@ pipeline
 # %%
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
+saved_mae = []
+saved_x_train_indices = []
+saved_x_test_indices = []
+random_states = range(10)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-pipeline.fit(X_train, y_train)
-
-hat_y_test = pipeline.predict(X_test)
-mae = mean_absolute_error(y_test, hat_y_test)
-print(f"Test mean absolute error: {mae:.2f}")
+for rs in random_states:
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=rs)
+    pipeline.fit(X_train, y_train)
+    hat_y_test = pipeline.predict(X_test)
+    mae = mean_absolute_error(y_test, hat_y_test)
+    saved_mae.append(mae)
+    saved_x_train_indices.append(X_train.index)
+    saved_x_test_indices.append(X_test.index)
 # %%
-from sklearn.model_selection import train_test_split
+# plot figure
+fig = plt.figure(figsize=(6, 3))
+gs = fig.add_gridspec(1, 4)
+ax1 = fig.add_subplot(gs[0, :3])
+ax2 = fig.add_subplot(gs[0, 3])
+score_point_size = 100
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
-pipeline.fit(X_train, y_train)
-score = pipeline.score(X_test, y_test)
-print(f"Test score: {score:.2f}")
+for i, rs in enumerate(random_states):
+    X_train_indices = saved_x_train_indices[i]
+    X_test_indices = saved_x_test_indices[i]
+    mae = saved_mae[i]
+    ax1 = plot_train_test_indices(X_train_indices, X_test_indices, ax1, y=-rs, lw=10)
+    if i >= 1:
+        ax2.scatter([0]*i, saved_mae[:i], c='blue', alpha=0.8, s=score_point_size, marker="_")
+    mae_scatter = ax2.scatter([0], [mae], c='black', alpha=0.8, s=score_point_size, marker="_")
+    mae_label = ax2.annotate(f"{mae:.2f}", (0, mae), textcoords="offset points", xytext=(10,0), ha='left')
+    # formattting
+    ax1.set_yticks([-rs for rs in random_states])
+    ax1.set_yticklabels(random_states)
+    ax1.set_ylim(-len(random_states), 1)
+
+    # Add labels and legend
+    ax1.set_ylabel('Random seed')
+
+    ax2.set_ylim(min(saved_mae)-0.1, max(saved_mae)+0.1)
+    ax2.set_xticks([])
+    ax2.yaxis.tick_right()
+    ax2.grid(False)
+    ax2.yaxis.set_label_position("right")
+    ax2.set_ylabel('Mean Absolute Error')
+    plt.savefig(DIR2FIG/f'train_test_split_visualization_seed_{rs}.png')
+    mae_label.remove()
+    mae_scatter.remove()
+    
