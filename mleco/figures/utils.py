a67a200b30224e2d21ce155e4c4cddde86eec8ee
Matthieu Doutreligne <matt.dout@gmail.com>
2024-12-20 20:17:15 +0100

    wip cross validation intutition

--- mleco/figures/utils.py
+++ mleco/figures/utils.py
@@ -1,3 +1,37 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+cmap_data = plt.cm.Paired
+cmap_cv = plt.cm.coolwarm
+
+
+def plot_train_test_indices(X_train_indices, X_test_indices, ax, y=0, lw=10):
+    indices = np.array([np.nan] * (len(X_train_indices) + len(X_test_indices)))
+    indices[X_train_indices] = 1
+    indices[X_test_indices] = 0
+
+    # Visualize the results
+    ax.scatter(
+        range(len(indices)),
+        [y] * len(indices),
+        c=indices,
+        marker="_",
+        lw=lw,
+        cmap=cmap_cv,
+        vmin=-0.2,
+        vmax=1.2,
+    )
+    # Formatting
+    ax.set(
+        xlabel="Sample index",
+        # yticks=[y],
+        xlim=[0, len(indices)],
+    )
+    # ax.set_title("{}".format(type(cv).__name__), fontsize=15)
+    return ax
+
+
 def plot_cv_indices(cv, X, y, group, ax, n_splits, lw=10):
     """Create a sample plot for indices of a cross-validation object."""
     use_groups = "Group" in type(cv).__name__
