7d70369915bf4a9167a6e3b666be16d854f25ade
Matthieu Doutreligne <m.doutreligne@has-sante.fr>
2025-01-17 17:45:29 +0100

    event_study_practical_session: wip

--- notebooks/event_studies_practical_session.py
+++ notebooks/event_studies_practical_session.py
@@ -13,148 +13,81 @@
 # ---
 
 # %% [markdown]
-# # Comparative Politics and the Synthetic Control Method
-# ### Abadie, Diamond and Hainmueller
+# # Plan of the practical session: 
+#  - Application of the ITS method with a simple SARIMA model with the `statsmodels` and a more elaborated model with `causalimpact` packages. 
+#  - Application of the synthetic control using the `pysyncon` package. 
+# 
+# # Data
+# - STEP 1. Replicating the original CITS analyses by [Humphreys et al. (2017)](https://jamanetwork.com/journals/jamainternalmedicine/fullarticle/2582988) using four comparison states (New York, New Jersey, Ohio, Virginia). 
+# - STEP 2. Extending the original CITS analyses by [Humphreys et al. (2017)](https://jamanetwork.com/journals/jamainternalmedicine/fullarticle/2582988) using synthetic control methods (15 comparison states in the donor pool). 
+# The code is inspired by the analyses in the original papers from [Degli Esposti et al., 2020](https://academic.oup.com/ije/article/49/6/2010/5917161#supplementary-data). It uses the data from (Bonander et al., 2021)[https://academic.oup.com/aje/article-abstract/190/12/2700/6336907].
 
-# %% [markdown]
-# This notebook reproduces the weights from the 2015 paper "Comparative Politics and the Synthetic Control Method" by Alberto Abadie, Alexis Diamond and Jens Hainmueller. The study data is contained in the file `../data/germany.csv` (more detailed information on this dataset is available in the appendix of the cited paper). This notebook is taken from the `pysyncon` [package](https://github.com/sdfordham/pysyncon).
-
-# %%
+#  %% 
+import numpy as np
 import pandas as pd
-from pysyncon import Dataprep, Synth
-
-# %% [markdown]
-# The study is carried out in two steps. In each step we prepare the study data using a `Dataprep` object that describes all the details needed to define the synthetic control study. This works similarly to the `dataprep` method in the `R` package `synth`.
-#
-# In the first run, the optimisation is carried out over the period 1981 to 1991, and the $V$ matrix obtained in this optimisation is then used in the second run, where the optimisation is carried out over the period 1960 to 1990. This serves to enforce that the predictor importances used in the final optimisation are those of the eighties.
-#
-# In each case, we supply the `Dataprep` object to a `Synth` object. In the second run, we can provide a custom $V$ matrix with the `custom_V` option.
-#
-# (For an explanation of each of the `Dataprep` arguments, see the package [documentation](https://sdfordham.github.io/pysyncon/dataprep.html#pysyncon.Dataprep)).
+import matplotlib.pyplot as plt
+import seaborn as sns
+from skrub import TableReport
+from datetime import datetime
 
 # %%
-df = pd.read_csv("../data/germany.csv")
+URL2DATA = "https://raw.githubusercontent.com/strayMat/causal-ml-course/refs/heads/main/data/homicides_data.csv"
 
-dataprep_train = Dataprep(
-    foo=df,
-    predictors=["gdp", "trade", "infrate"],
-    predictors_op="mean",
-    time_predictors_prior=range(1971, 1981),
-    special_predictors=[
-        ("industry", range(1971, 1981), "mean"),
-        ("schooling", [1970, 1975], "mean"),
-        ("invest70", [1980], "mean"),
-    ],
-    dependent="gdp",
-    unit_variable="country",
-    time_variable="year",
-    treatment_identifier="West Germany",
-    controls_identifier=[
-        "USA",
-        "UK",
-        "Austria",
-        "Belgium",
-        "Denmark",
-        "France",
-        "Italy",
-        "Netherlands",
-        "Norway",
-        "Switzerland",
-        "Japan",
-        "Greece",
-        "Portugal",
-        "Spain",
-        "Australia",
-        "New Zealand",
-    ],
-    time_optimize_ssr=range(1981, 1991),
+TREATED_COLOR = plt.get_cmap("tab10")(0)  # Default matplotlib blue
+CONTROL_COLOR = plt.get_cmap("tab10")(1)  # Default matplotlib orange
+
+COL_TREATMENT = "treatdummy"
+COL_TREATED_UNIT = "Case"
+TREATMENT_DATE = datetime(2005, 10, 1)
+# %%
+# Load the data 
+homicides = pd.read_csv(URL2DATA, index_col=0)
+# add a column with the Date format from the year and month columns
+homicides["Month.string"] = homicides["Month.code"].apply(lambda x: f"{x:02d}")
+homicides["Date"] = pd.to_datetime(homicides["Year"].astype(str)+"-"+homicides["Month.string"], format="%Y-%m")
+TableReport(homicides)
+# %%
+# %%
+states_subset_for_its = ["Florida","New York", "New Jersey", "Ohio", "Virginia"]
+homicides_subset_for_its = homicides[homicides["State"].isin(states_subset_for_its)]
+print(homicides_subset_for_its["State"].unique())
+# %%
+# Plotting the data
+fig, ax = plt.subplots(1, 1, figsize=(16, 8))
+sns.lineplot(data=homicides_subset_for_its, x="Date", y="HomicideRates", hue="State", ax=ax, marker="o")
+plt.show()
+# %%
+# Plotting the data by treatment status
+fig, ax = plt.subplots(1, 1, figsize=(16, 8))
+sns.lineplot(data=homicides_subset_for_its, x="Date", y="HomicideRates", hue=COL_TREATED_UNIT, ax=ax, marker="o", palette={0: CONTROL_COLOR, 1: TREATED_COLOR})
+# plot treatment date
+ax.axvline(TREATMENT_DATE, color="black", linestyle="--")
+plt.show()
+# %% 
+
+pretreatment_data = homicides_subset_for_its[homicides_subset_for_its["Date"] < TREATMENT_DATE]
+posttreatment_data = homicides_subset_for_its[homicides_subset_for_its["Date"] >= TREATMENT_DATE]
+pretreatment_treated = pretreatment_data[pretreatment_data[COL_TREATMENT] == 1]
+
+# %%
+# STEP 1: ITS with SARIMA model
+# %%
+from statsmodels.tsa.arima.model import ARIMA
+arima = ARIMA(pretreatment_treated["HomicideRates"], order=(1, 0, 0))
+arima_fit = arima.fit()
+print(arima_fit.summary())
+y_pred_w_ci = arima_fit.get_prediction(
+    start=0, end=(N_OBS - 1)
 )
-
-print(dataprep_train)
-
-# %%
-synth_train = Synth()
-synth_train.fit(dataprep=dataprep_train)
+ci = y_pred_w_ci.conf_int()
+fig, ax = plt.subplots(1, 1, figsize=(10, 5))
 
 # %%
-dataprep = Dataprep(
-    foo=df,
-    predictors=["gdp", "trade", "infrate"],
-    predictors_op="mean",
-    time_predictors_prior=range(1981, 1991),
-    special_predictors=[
-        ("industry", range(1981, 1991), "mean"),
-        ("schooling", [1980, 1985], "mean"),
-        ("invest80", [1980], "mean"),
-    ],
-    dependent="gdp",
-    unit_variable="country",
-    time_variable="year",
-    treatment_identifier="West Germany",
-    controls_identifier=[
-        "USA",
-        "UK",
-        "Austria",
-        "Belgium",
-        "Denmark",
-        "France",
-        "Italy",
-        "Netherlands",
-        "Norway",
-        "Switzerland",
-        "Japan",
-        "Greece",
-        "Portugal",
-        "Spain",
-        "Australia",
-        "New Zealand",
-    ],
-    time_optimize_ssr=range(1960, 1990),
-)
+# Compare with a SARIMAX model (should be close from causal impact)
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 
-print(dataprep)
+# %% 
+# Make the analysis super easy with causal impact
 
-# %%
-synth = Synth()
-synth.fit(dataprep=dataprep, custom_V=synth_train.V)
-
-synth.weights()
-
-# %% [markdown]
-# The synthetic control obtained from the optimisation is: $$\text{Synthetic Control} = 0.216 \times \text{USA} + 0.415 \times \text{Austria} + 0.098 \times \text{Nederlands} + 0.108 \times \text{Switzerland} + 0.163 \times \text{Japan}.$$
-
-# %% [markdown]
-# The `path_plot` method shows the path of the treated unit and the synthetic control over time.
-
-# %%
-synth.path_plot(time_period=range(1960, 2004), treatment_time=1990)
-
-# %% [markdown]
-# The `gaps_plot` method shows the gaps (the difference between the treated unit and the synthetic control) over time.
-
-# %%
-synth.gaps_plot(time_period=range(1960, 2004), treatment_time=1990)
-
-# %% [markdown]
-# The summary function give more information on the predictor values. The first column shows the value of the $V$ matrix for each predictor, the column 'treated' shows the mean value of each predictor for the treated unit over the time period `time_predictors_prior`, the column 'synthetic' shows the mean value of each predictor for the synthetic control over the time period `time_predictors_prior` and finally the column 'sample mean' shows the sample mean of that predictor for all control units over the time period `time_predictors_prior` i.e. this is the same as the synthetic control with all weights equal.
-
-# %%
-synth.summary()
-
-# %% [markdown]
-# Compute the average treatment effect on the treated unit (ATT) over the post-treatment time period. This method returns a standard error also.
-
-# %%
-synth.att(time_period=range(1990, 2004))
-
-# %% [markdown]
-# Calculate 95% confidence intervals for the treatment effect for time periods $t=1991, \ldots, 2000$.
-
-# %%
-synth.confidence_interval(
-    alpha=0.05,
-    time_periods=[1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000],
-    custom_V=synth_train.V,
-    tol=0.01,
-    verbose=False,
-)
+# %% 
+# STEP 2: Synthetic control method
